825bbbdf0f7dc2f8eb90b49aaf49039992cd96ad
Kaustubh Funde <kaustubh.funde@nutanix.com>
2024-07-15 16:16:57 -0700

    Created basic layout.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Row, Col } from 'antd';
+import DeviceAvailability from './components/DeviceAvailability';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Row justify="space-between" gutter={4}>
+        <Col xs={24} md={12} lg={3}>
+          <DeviceAvailability />
+        </Col>
+        <Col xs={24} md={12} lg={9}>
+          <DeviceAvailability />
+        </Col>
+        <Col xs={24} md={12} lg={3}>
+          <DeviceAvailability />
+        </Col>
+        <Col xs={24} md={12} lg={9}>
+          <DeviceAvailability />
+        </Col>
+      </Row>
+    )
 }
 
 export default App
---
+++ frontend/src/components/DeviceAvailability/styles.jsx
@@ -0,0 +1,4 @@
+export const styles = {
+    backgroundColor: '#3b3b3b',
+    padding: '0px'
+}
