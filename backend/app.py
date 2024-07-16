fb4eb6f56614c16047cd485922657bd1d07edc0a
Avi Khandelwal <avi.khandelwal@kraferd.com>
2024-07-15 20:11:42 -0700

    made pie chart and donut + connected to backend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import time, pandas as pd, random
+from flask_cors import CORS
 
 app = Flask(__name__)
-df = pd.read_csv('../data/network_dataset.csv')
+CORS(app, resources={r"/*": {"origins": "*"}})
+df = pd.read_csv('./network_dataset.csv')
 
-@app.route('/', methods=['GET'])
-def send_next_request():
-    req = df.sample().to_dict(orient='records')[0]
-    print(req)
-
-while(True):
-    time.sleep(random.randint(1, 5))
-    send_next_request()
+@app.route('/network-counts', methods=['GET'])
+def send_network_counts():
+    return jsonify(df.groupby('attack').size().to_dict())
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,46 +5,29 @@ import { Row, Col } from 'antd';
 import DeviceAvailability from './components/DeviceAvailability';
 import './App.css'
 import NetworkAvailibilityDonut from './components/NetworkAvailabilityDonut'
+import NetworkPieChart from './components/NetworkPieChart'
 
-const pieChartData = [
-  {
-    name: "new",
-    total: 40
-  },
-  {
-    name: "evaluating",
-    total: 25
-  },
-  {
-    name: "ongoing",
-    total: 22
-  },
-  {
-    name: "finished",
-    total: 22
-  },
-  {
-    name: "archived",
-    total: 10
-  }
-]
 
 function App() {
   return (
-      <Row justify="space-between" gutter={4}>
-        <Col xs={24} md={12} lg={3}>
-          <DeviceAvailability />
-        </Col>
-        <Col xs={24} md={12} lg={9}>
-          <DeviceAvailability />
-        </Col>
-        <Col xs={24} md={12} lg={3}>
-          <DeviceAvailability />
-        </Col>
-        <Col xs={24} md={12} lg={9}>
-          <DeviceAvailability />
-        </Col>
-      </Row>
+      // <Row justify="space-between" gutter={4}>
+      //   <Col xs={24} md={12} lg={3}>
+      //     <DeviceAvailability />
+      //   </Col>
+      //   <Col xs={24} md={12} lg={9}>
+      //     <DeviceAvailability />
+      //   </Col>
+      //   <Col xs={24} md={12} lg={3}>
+      //     <DeviceAvailability />
+      //   </Col>
+      //   <Col xs={24} md={12} lg={9}>
+      //     <DeviceAvailability />
+      //   </Col>
+      // </Row>
+      <>
+        <NetworkAvailibilityDonut/>
+        <NetworkPieChart/>
+      </>
     )
 }
 
