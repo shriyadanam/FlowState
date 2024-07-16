e9eae88e089ce947ab082f397c43f8d93d2e136d
Kaustubh Funde <kaustubh.funde@nutanix.com>
2024-07-16 11:23:14 -0700

    Added automated network requests and popup notifications.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 // React and useState import
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 // Ant Design imports
-import { Row, Col } from 'antd';
+import { Row, Col, notification } from 'antd';
 
 // CSS import
 import './App.css';
@@ -13,36 +13,75 @@ import Form from './components/Form';
 import TopIPs from './components/TopIPs';
 import Visualization from './components/Visualization/graph';
 import NetworkPieChart from './components/NetworkPieChart';
+import axios from 'axios';
+import { fontWeight } from '@mui/system';
 
+const Context = React.createContext({ name: 'Default' });
 // App component
 function App() {
+  const contextValue = useMemo(() => ({ notification: notification }), []);
+  const [api, contextholder] = notification.useNotification();
+
+  async function automatedReqest() {
+    return axios.get("http://localhost:5000/automated-network-request");
+  }
+
+  function getAutomatedRequests() {
+    setInterval(() => {
+      void (async () => {
+        try {
+          const res = await automatedReqest();
+          if (res.data.prediction === 'normal') {
+            return;
+          }
+          api.warning({
+            message: `Suspected ${res.data.prediction.toUpperCase()} attack`,
+            description: `From ${res.data.source} to ${res.data.destination}`,
+            placement: 'topRight',
+            icon: <img src="https://cdn1.iconfinder.com/data/icons/color-bold-style/21/08-1024.png" alt="alert" style={{height: '25px'}}/>,
+          });
+        }
+        catch(err) {
+          console.error("Error: ", err);
+        }
+      })();
+    }, 8000);
+  }
+
+  getAutomatedRequests();
+
   return (
     <div className="app-container">
-      <Row justify="space-between" gutter={24}>
-        <Col>
-          <div className="component-box">
-            <NetworkAvailibilityDonut />
-          </div>
-          <div className="component-box">
-            <NetworkPieChart />
-          </div>
-        </Col>
-        <Col>
-          <div className="component-box">
-            <TopIPs />
-          </div>
-        </Col>
-        <Col>
-          <div className="component-box">
-            <Form />
-          </div>
-        </Col>
-        <Col>
-          <div className="component-box">
-            <Visualization />
-          </div>
-        </Col>
-      </Row>
+      <Context.Provider value={contextValue}>
+        {contextholder}
+        <Row justify="space-between" gutter={24}>
+          <Col onClick={(e) => {
+            openNotification();
+          }}>
+            <div className="component-box">
+              <NetworkAvailibilityDonut />
+            </div>
+            <div className="component-box">
+              <NetworkPieChart />
+            </div>
+          </Col>
+          <Col>
+            <div className="component-box">
+              <TopIPs />
+            </div>
+          </Col>
+          <Col>
+            <div className="component-box">
+              <Form />
+            </div>
+          </Col>
+          <Col>
+            <div className="component-box">
+              <Visualization />
+            </div>
+          </Col>
+        </Row>
+      </Context.Provider>
     </div>
   );
 }
